3235850024c33cf0b50daecdba1926c68aa8a224
Vladislav Beldiman <vladislav.beldiman@gmail.com>
2021-04-23 19:24:09 +0100

    Download files from embed

---
+++ tema/cpin3l.py
@@ -0,0 +1,23 @@
+#L8 - IOT Active WEB server
+# LED rosu -  pin 3
+# LED verde - pin 5
+# LED blue  - pin 7
+import picamera
+import RPi.GPIO as GPIO
+
+file=open("/var/www/html/imag/imag1.jpg","wb")
+camera=picamera.PiCamera()
+camera.resolution =(320,240)  # 1024,768
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3,GPIO.OUT)
+GPIO.output(3,0) # sting LED
+GPIO.setwarnings(False)
+
+#camera.start_preview()
+#time.sleep(2)
+camera.capture(file)
+file.close()
+
+
+
+
