84d8d08d7e764bf1d2be9dfd6f60049b9a45fc93
Vladislav Beldiman <vladislav.beldiman@gmail.com>
2021-05-01 22:41:40 +0300

    Comment rpi GPIO calls

--- server/led.py
+++ server/led.py
@@ -1,11 +1,11 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import socket
 import time
 from camera import LED_PORT
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(37,GPIO.OUT)
-GPIO.output(37,1)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(37,GPIO.OUT)
+#GPIO.output(37,1)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect(('localhost', LED_PORT))
@@ -19,5 +19,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         except ConnectionResetError:
             break
 
-GPIO.output(37,0)
-GPIO.cleanup()
+#GPIO.output(37,0)
+#GPIO.cleanup()
